522fe1630f629566047021b28c74025b788566c8
gopalamn <gopalamn@umich.edu>
2021-01-06 12:24:47 -0600

    test deploy to heroku

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+# from flask import Flask
+# from flask_cors import CORS
+# import sys
+# import logging
+
+# def create_app():
+#     app = Flask(__name__)
+#     app.config.from_object('config')
+
+#     CORS(app)
+
+#     return app
---
+++ config.py
@@ -0,0 +1,29 @@
+import os
+import yaml
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    # Secret key for signing things
+    SECRET_KEY = b'd092b96b4edff56c1843aa87e0b1341b46310d4826c53a21'
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+
